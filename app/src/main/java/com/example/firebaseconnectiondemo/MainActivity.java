279ac2fa273985dd6a4a71e040f1136420033126
GeoVista <geovista_andriod_dev@outlook.com>
2026-04-25 09:59:57 +0530

    Changed in MainActivity

--- app/src/main/java/com/example/firebaseconnectiondemo/MainActivity.java
+++ app/src/main/java/com/example/firebaseconnectiondemo/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.firebaseconnectiondemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -10,11 +13,14 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore firestore;
 
+    Button button;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button=findViewById(R.id.button);
         firestore = FirebaseFirestore.getInstance();
 
         User user = new User();
@@ -31,5 +37,9 @@ public class MainActivity extends AppCompatActivity {
                 .addOnFailureListener(e -> {
                     e.printStackTrace();
                 });
+
+        button.setOnClickListener(view -> {
+            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show();
+        });
     }
 }
